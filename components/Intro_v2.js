ed0cda50b45bd37156229861c35bdab1813388f4
Lucas Martins <lksmartins11@gmail.com>
2021-04-12 17:31:32 -0300

    parallax style update

--- components/Intro_v2.js
+++ components/Intro_v2.js
@@ -59,7 +59,7 @@ export default function Intro(){
             
             <div className="frame parallax-item" data-speed="0" />
             <div className="logo parallax-item"  data-speed="80" />
-            <div className="robot parallax-item" data-speed="10" />
+            <div className="robot parallax-item" data-speed="40" />
             <div className="cards parallax-item" data-speed="50" />
             <div className="bg parallax-item"    data-speed="30" />
 
