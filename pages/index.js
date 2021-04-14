6de7e2726b2939576405e03710c84cc3b7fcea93
Lucas Martins <lksmartins11@gmail.com>
2021-04-14 11:11:15 -0300

    background update

--- pages/index.js
+++ pages/index.js
@@ -15,26 +15,28 @@ export default function Index() {
 
         <div className="container">
 
-            <Section className="section center mb-7">
+            <Section className="section center">
                 <h2 className="title mb-4">Available for Download (Alpha version)</h2>
                 <a className="btn btn-lg">Download for Windows</a>
             </Section>
 
-            <Section className="section mb-7">
+            <Section className="section">
                 <div className="fluid-media">
                     <iframe src="https://www.youtube-nocookie.com/embed/cXWlgP5hZzc" frameBorder="0" allowFullScreen=""></iframe>
                 </div>
-            </Section>            
+            </Section>  
 
-            <Section className="section center mb-7">
+            <Section className="section center">
+                <h3 className="mb-2">Latest News</h3>
+                <News />
+            </Section>          
+
+            <Section className="section center">
                 <h3 className="mb-2">Gallery</h3>
                 <Gallery />
             </Section>
 
-            <Section className="section center">
-                <h3 className="mb-2">News</h3>
-                <News />
-            </Section>
+            
 
         </div>
 
