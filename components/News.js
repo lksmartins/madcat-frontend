544c1169c0b982a27dc7cafe465936ce179ce07a
Lucas Martins <lksmartins11@gmail.com>
2021-04-15 11:34:29 -0300

    news updated

--- components/News.js
+++ components/News.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function News(props){
 	
 	var newsElements = props.articles ? props.articles : [
-		{title: 'Título Exemplo 1', content: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'},
-		{title: 'Título Exemplo 2', content: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'},
-		{title: 'Título Exemplo 3', content: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'},
-		{title: 'Título Exemplo 4', content: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'}
+		{title: 'Título Exemplo 1', preview: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'},
+		{title: 'Título Exemplo 2', preview: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'},
+		{title: 'Título Exemplo 3', preview: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'},
+		{title: 'Título Exemplo 4', preview: 'lorem ipsum dolor sit amet...', img: 'http://www.firewatchgame.com/screenshots/firewatch-e3-2.jpg'}
 	]
 
     return (
@@ -14,10 +14,13 @@ export default function News(props){
 			
 			{
 				newsElements.map((news, index)=>{
-					return (<div key={index}>
-						<img src={news.img} />
+					return (<div className="news-element" key={index}>
+						<div className="img">
+							<img src={news.img} />
+						</div>
+						<div className="date">{news.formatted_date}</div>
 						<h3 className="title">{news.title}</h3>
-						<p>{news.content}</p>
+						<p>{news.preview}</p>
 					</div>)
 				})
 			}
